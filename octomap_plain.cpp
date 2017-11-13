03fd4d3cfbe7cb6cec32148fd98a376a5d1341ff
Hafiz Muhammad Luqman <hafizlouqman@gmail.com>
2017-11-13 14:50:35 +0100

    Update: point by point points added

--- octomap_plain.cpp
+++ octomap_plain.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 using namespace octomap;
+
 int main()
 {
     pcl::PointCloud<pcl::PointXYZ> cloud ;
@@ -15,30 +16,45 @@ int main()
         PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
         return (-1);
     }
- //   std::cout << "Loaded  points "<<cloud->size();
+    std::cout << "Loaded  points "<<cloud.size()<<std::endl;
     octomap::OcTree tree (0.1);  // create empty tree with resolution 0.1
     octomap::point3d sensorOrigin(0.0, 0.0, 0.0);
-    octomap::Pointcloud octoPointCloud;
 
+    octomap::KeySet occupied_cells;
+    // define a std pair to get results of insert function later
+    std::pair<octomap::KeySet::iterator,bool> ret = std::make_pair(occupied_cells.begin(), false);
     for(int i = 0; i < int(cloud.size()); ++i)
     {
         octomap::point3d point(cloud[i].x, cloud[i].y, cloud[i].z);
-        octoPointCloud.push_back(point);
+        // occupied endpoint
+        octomap::OcTreeKey key;
+        if (tree.coordToKeyChecked(point, key))
+        {
+            ret.second = false;
+            // get the results of insert function - returns false if key is already present
+            ret = occupied_cells.insert(key);
+            if (ret.second)
+                tree.updateNode(key, true);
+        }
+
     }
 
-    tree.insertPointCloud(octoPointCloud, sensorOrigin);
-
-    for(octomap::OcTree::tree_iterator it = tree.begin_tree(),
-           end=tree.end_tree(); it!= end; ++it)
+    for(octomap::OcTree::tree_iterator it = tree.begin_tree(), end=tree.end_tree(); it!= end; ++it)
     {
-      //manipulate node, e.g.:
-      std::cout << "Node center: " << it.getCoordinate() << std::endl;
-      std::cout << "Node size: " << it.getSize() << std::endl;
+            octomap::OcTreeKey key;
+            key = it.getKey();
+//            octomap::point3d poin = tree.keyToCoord(key);
+            std::cout <<"Key = "<<key.k[0]<<" "<<key.k[1]<<" "<<key.k[2]<<std::endl;
+    }
+
+/*
+    for(octomap::OcTree::tree_iterator it = tree.begin_tree(), end=tree.end_tree(); it!= end; ++it)
+    {
+//      std::cout << "Node center: " << it.getCoordinate() << std::endl;
+  //    std::cout << "Node size: " << it.getSize() << std::endl;
 //      std::cout << "Node value: " << it->getValue() << std::endl;
     }
-
-    std::cout << endl;
-    std::cout << "performing some queries:" << endl;
+*/
 
     octomap::point3d query (0., 0., 0.);
     octomap::OcTreeNode* result = tree.search (query);
@@ -46,10 +62,6 @@ int main()
 
     std::cout << endl;
     tree.writeBinary("simple_tree.bt");
-    std::cout << "wrote example file simple_tree.bt" << endl << endl;
-    std::cout << "now you can use octovis to visualize: octovis simple_tree.bt"  << endl;
-    std::cout << "Hint: hit 'F'-key in viewer to see the freespace" << endl  << endl;
-
 
     return 0;
 }
